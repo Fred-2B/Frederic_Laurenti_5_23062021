7ecf7bbdf2cc22fd8347c4574aff4bbc16508844
Fred-2B <flaurenti@free.fr>
2021-06-29 15:42:15 +0200

    depot projet

---
+++ js/produit.js
@@ -0,0 +1,26 @@
+(async function() {
+    const teddies = await getTeddies()
+    for (teddy of teddies) {
+        displayTeddy(teddy)
+    }
+  })()
+  
+  /* Appeler les données via API */
+  function getTeddies() {
+    return fetch("http://localhost:3000/api/cameras")
+        .then((responseHttp) => responseHttp.json())        
+  }
+  
+  /* Récupérer les informations contenues dans le tableau*/
+  function displayTeddy(teddy) {
+    const templateElt = document.getElementById("templateArticle")
+    // const cloneElt = document.importNode(templateElt.content, true)
+  
+    cloneElt.getElementById("blog__image").src = teddy.imageUrl
+    cloneElt.getElementById("blog__title").textContent = teddy.name
+    cloneElt.getElementById("blog__price").textContent = teddy.price / 100 + (' €')
+  
+    cloneElt.getElementById("blog__lien").href = "detail.html?id=" + teddy._id
+  
+    document.getElementById("produits").appendChild(cloneElt)
+  }
